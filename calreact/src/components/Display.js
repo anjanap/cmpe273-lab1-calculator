089bbcb13c2d7d3a354ffe4b77f6f0bed3cd44c5
anjanap <anjana.pradeep@sjsu.edu>
2017-10-04 23:33:18 -0700

    ui changes

--- calreact/src/components/Display.js
+++ calreact/src/components/Display.js
@@ -30,20 +30,45 @@ class Display extends Component {
           <center>
           <h1><b>CALCULATOR</b></h1><br/>
           <form>
+          <div className="col-sm-12">
+           <div className="col-sm-4">
            <input id="number1" type="number" pattern="^[0-9]*$" placeholder="Number 1" onChange={(event)=>{
-                                         this.setState({inp1: event.target.value});}} value={this.state.inp1}></input>
+                                         this.setState({inp1: event.target.value});}} value={this.state.inp1}></input></div>
+          <div className="col-sm-2"></div>
+           <div className="col-sm-4">
            <input id="number2" type="number" pattern="^[0-9]*$" placeholder="Number 2" onChange={(event)=>{
-                                         this.setState({inp2: event.target.value});}} value={this.state.inp2}></input><br/><br/>
+                                         this.setState({inp2: event.target.value});}} value={this.state.inp2}></input><br/><br/></div>
+          </div>
 
-          <input type="button" id="add" value="+" onClick={()=> this.handleSubmit(this.state,'+')}></input>
-          <input type="button" id="sub" value="-" onClick={()=> this.handleSubmit(this.state,'-')}></input><br/>
+          <div className="col-sm-12">
+          <div className="col-sm-4"></div>
+          <div className="col-sm-2">
+          <input type="button" className="btn btn-lg btn-primary" id="add" value="+" onClick={()=> this.handleSubmit(this.state,'+')}></input>
+          </div>
+          <div className="col-sm-2">
+          <input type="button" className="btn btn-lg btn-primary" id="sub" value="-" onClick={()=> this.handleSubmit(this.state,'-')}></input>
+          </div>
+          <div className="col-sm-4"></div>
+          </div>
 
-          <input type="button" id="mul" value="*" onClick={()=> this.handleSubmit(this.state,'*')}></input>
-          <input type="button" id="div" value="/" onClick={()=> this.handleSubmit(this.state,'/')}></input><br/><br/>
+          <div className="col-sm-12">
+          <div className="col-sm-4"></div>
+          <div className="col-sm-2"><br/>
+          <input type="button" className="btn btn-lg btn-primary" id="mul" value="*" onClick={()=> this.handleSubmit(this.state,'*')}></input>
+          </div>
+          <div className="col-sm-2"><br/>
+          <input type="button" className="btn btn-lg btn-primary" id="div" value="/" onClick={()=> this.handleSubmit(this.state,'/')}></input>
+          </div>
+          <div className="col-sm-4"></div>
+          </div>
 
-          <input type="button" id="clear" value="clear" onClick={() => this.setState({data:"",
+          <div className="col-sm-12">
+          <br/>
+          <input type="button" className="btn btn-primary" id="clear" value="clear" onClick={() => this.setState({data:"",
                                                                                       inp1:"",
                                                                                       inp2:""})}></input><br/><br/>
+          </div>
+
           <b>RESULT:</b> <input type="text" id="outString" value={this.state.data}></input><br/>
          </form>
          </center>
