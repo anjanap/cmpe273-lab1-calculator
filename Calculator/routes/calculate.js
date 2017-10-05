6d2003a7e903770ffef5333eb21669fae51e6648
anjanap <anjana.pradeep@sjsu.edu>
2017-10-04 21:44:27 -0700

    node changes

--- Calculator/routes/calculate.js
+++ Calculator/routes/calculate.js
@@ -2,18 +2,17 @@ var express=require('express');
 var router=express.Router();
 
 router.post('/result',function(req,res){
-	var inp1=req.body.inp1;
-	var inp2=req.body.inp2;
+	var num1=req.body.inp1;
+	var num2=req.body.inp2;
 	var op=req.body.op;
 	if(op=='+')
-		r=parseFloat(inp1)+parseFloat(inp2);
+		r=parseFloat(num1)+parseFloat(num2);
 	if(op=='-')
-		r=inp1-inp2;
+		r=num1-num2;
 	if(op=='*')
-		r=inp1*inp2;
+		r=num1*num2;
 	if(op=='/')
-		r=inp1/inp2;
-	//console.log(r);
+		r=num1/num2;
 	res.status(201).json({output:r});
 	});
 
